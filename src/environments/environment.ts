815eda4d3101fd4b8246ce5a8463b90c98a234a5
Penkie <oscar@oscarprince.ch>
2025-08-21 15:31:29 +0200

    refactor(build): change api url in env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     kc_url: 'https://idp.oscarprince.ch',
     kc_realm: 'budgetanalytics',
     kc_client: 'budgetanalytics-web',
-    api_url: 'https://api.budgetanalytics.app',
+    api_url: 'https://new.api.budgetanalytics.app',
     path: 'https://budgetanalytics.app',
-    api_url_pattern: /^(https:\/\/api.budgetanalytics.app)(\/.*)?$/i,
+    api_url_pattern: /^(https:\/\/new.api.budgetanalytics.app)(\/.*)?$/i,
 };
