2b5f4e791f286adeefb60b295ece97138dec555d
Penkie <oscar@oscarprince.ch>
2025-08-21 16:02:13 +0200

    fix(layout): close mobile menu when navigating

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,6 +1,13 @@
-import { Component, computed, signal } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Component, computed, effect, inject, signal } from '@angular/core';
+import {
+    NavigationEnd,
+    Router,
+    RouterModule,
+    RouterOutlet,
+} from '@angular/router';
 import { NotificationContainerComponent } from '../common/components/notification/notification';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { filter } from 'rxjs';
 
 @Component({
     selector: 'app-layout',
@@ -14,6 +21,18 @@ export class Layout {
 
     // Computed property for easier template access
     isMobileSidebarOpen = computed(() => this.mobileMenuOpen());
+
+    private readonly router = inject(Router);
+
+    constructor() {
+        this.router.events
+            .pipe(filter((event) => event instanceof NavigationEnd))
+            .subscribe(() => {
+                if (this.mobileMenuOpen()) {
+                    this.closeMobileSidebar();
+                }
+            });
+    }
 
     toggleMobileSidebar(): void {
         this.mobileMenuOpen.update((open) => !open);
