21d89b96793057736780929063e2d2a9fb671574
Penkie <oscar@oscarprince.ch>
2025-08-21 15:39:43 +0200

    feat(build): add staging env

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    kc_url: 'https://idp.oscarprince.ch',
+    kc_realm: 'budgetanalytics',
+    kc_client: 'budgetanalytics-web',
+    api_url: 'https://new.api.budgetanalytics.app',
+    path: 'https://staging.budgetanalytics.app',
+    api_url_pattern: /^(https:\/\/new.api.budgetanalytics.app)(\/.*)?$/i,
+};
