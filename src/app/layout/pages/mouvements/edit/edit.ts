921ae095a0bf7eb71de58c729abbf657cabfbd1f
Penkie <oscar@oscarprince.ch>
2025-08-25 09:38:03 +0200

    feat(mouvements): wip edit form

--- src/app/layout/pages/mouvements/edit/edit.ts
+++ src/app/layout/pages/mouvements/edit/edit.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import {
     PageHeader,
     PagePath,
@@ -6,6 +6,8 @@ import {
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router } from '@angular/router';
 import { map } from 'rxjs';
+import { AccountService } from '../../../../common/services/account.service';
+import { CategoryService } from '../../../../common/services/categorie.service';
 
 @Component({
     selector: 'app-edit',
@@ -15,9 +17,24 @@ import { map } from 'rxjs';
 })
 export class EditMouvement {
     private readonly activatedRoute = inject(ActivatedRoute);
+    private readonly accountsService = inject(AccountService);
+    private readonly categoryService = inject(CategoryService);
     private readonly transactionId = toSignal(
         this.activatedRoute.paramMap.pipe(map((param) => param.get('id'))),
         { initialValue: null }
+    );
+
+    public readonly selectedType = signal('expense');
+    public readonly selectedAccount = signal('');
+    public readonly selectedCategory = signal('');
+
+    public readonly accounts = toSignal(this.accountsService.getAccounts(), {
+        initialValue: null,
+        requireSync: false,
+    });
+    public readonly categories = toSignal(
+        this.categoryService.getCategories(),
+        { initialValue: null, requireSync: false }
     );
 
     public readonly pagePath: Array<PagePath> = [
